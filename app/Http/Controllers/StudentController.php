c629382652de06b5d1da873c8b4b854288b937d3
quinantotocv.214.stud@cdd.edu.ph <charisse09@gmail.com>
2024-07-16 14:45:25 +0800

    model1Quinantoto

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /** 
+     * Display a listing of the resorce.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show form for creating a new resource.
+    */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+}
+
+
